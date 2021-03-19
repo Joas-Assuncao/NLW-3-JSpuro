e4cf58266b3104da604ba77eb0185fd2977bc93c
Joas <joassalvior@gmail.com>
2021-03-18 21:18:22 -0300

    projeto NLW quase finalizado

---
+++ src/database/fakedata.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-15.7951568",
+    lng: "-47.88394991",
+    name: "Lar das Crianças",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      
+      "https://blog.conexasaude.com.br/wp-content/uploads/2019/05/thinkstockphotos-944585218.jpg",
+
+      "https://static.wixstatic.com/media/1553af_5d3ec84bd8e24d74ba65577c071e7136~mv2.jpg/v1/fill/w_1200,h_800,al_c,q_90/1553af_5d3ec84bd8e24d74ba65577c071e7136~mv2.jpg"
+
+    ],
+    instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 8h até 18h",
+    opening_on_weekends: "1"
+
+  },
+
+  {
+    id: 2,
+    lat: "-15.7951568",
+    lng: "-47.89394991",
+    name: "Casa Esperança",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      
+      "https://blog.conexasaude.com.br/wp-content/uploads/2019/05/thinkstockphotos-944585218.jpg",
+
+      "https://static.wixstatic.com/media/1553af_5d3ec84bd8e24d74ba65577c071e7136~mv2.jpg/v1/fill/w_1200,h_800,al_c,q_90/1553af_5d3ec84bd8e24d74ba65577c071e7136~mv2.jpg"
+
+    ],
+    instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 8h até 18h",
+    opening_on_weekends: "0"
+
+  }
+]
+
